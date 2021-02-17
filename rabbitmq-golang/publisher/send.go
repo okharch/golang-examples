a5773d785828ed146fa5be635daef0d5e80011f1
Oleksandr Kharchenko <okharch@gmail.com>
2021-02-17 10:54:33 +0200

    queue declared and hello world message sent

--- rabbitmq-golang/publisher/send.go
+++ rabbitmq-golang/publisher/send.go
@@ -1,14 +1,15 @@
 package main
 
 import (
-  "log"
+	"log"
 
-  "github.com/streadway/amqp"
+	"github.com/streadway/amqp"
 )
+
 func failOnError(err error, msg string) {
-  if err != nil {
-    log.Fatalf("%s: %s", msg, err)
-  }
+	if err != nil {
+		log.Fatalf("%s: %s", msg, err)
+	}
 }
 func main() {
 	// establish connection to rabbitmq as a guest user
@@ -22,4 +23,28 @@ func main() {
 	failOnError(err, "Failed to open a channel")
 	defer ch.Close()
 	log.Println("channel established")
+	// To send, we must declare a queue for us to send to; then we can publish a message to the queue:
+	q, err := ch.QueueDeclare(
+		"hello", // name
+		false,   // durable
+		false,   // delete when unused
+		false,   // exclusive
+		false,   // no-wait
+		nil,     // arguments
+	)
+	failOnError(err, "Failed to declare a queue")
+	// Declaring a queue is idempotent - it will only be created if it doesn't exist already. The message content is a byte array, so you can encode whatever you like there.
+	log.Println("hello queue declared!")
+	// send hello, world! message
+	body := "Hello World!"
+	err = ch.Publish(
+		"",     // exchange
+		q.Name, // routing key
+		false,  // mandatory
+		false,  // immediate
+		amqp.Publishing{
+			ContentType: "text/plain",
+			Body:        []byte(body),
+		})
+	failOnError(err, "Failed to publish a message")
 }
