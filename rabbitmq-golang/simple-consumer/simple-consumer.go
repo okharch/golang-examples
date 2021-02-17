8c91c7d70bc93d2342610cd0d22da024236470ba
Oleksandr Kharchenko <okharch@gmail.com>
2021-02-17 14:43:32 +0200

    no need for select. waiting for signal from channel does it

--- rabbitmq-golang/simple-consumer/simple-consumer.go
+++ rabbitmq-golang/simple-consumer/simple-consumer.go
@@ -42,10 +42,7 @@ func main() {
 		log.Printf("running forever")
 		c := make(chan os.Signal, 1)
 		signal.Notify(c, os.Interrupt)
-		select {
-		case <-c:
-			break
-		}
+		<-c
 	}
 
 	log.Printf("shutting down")
